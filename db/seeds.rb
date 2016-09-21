fd1b4843db9f1d65edc2696fbc59908454dc56c8
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-09-21 15:04:28 -0400

    2016 seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,131 @@ Lookslike.destroy_all
 Lookslike.create!(year: 2016, name: 'Jim Caldwell',
 looks_like: 'Looks like the nervous armored car driver transporting his largest and last delivery, suspicious his new partner might be up to something. Jay Cutler is his new partner.',
 photo_url: 'http://i.imgur.com/lf9MJYM.jpg')
+
+Lookslike.create!(name: 'Jay Cutler',
+looks_like:'Looks like the guy trying to break into a house on an alarm company commercial.',
+photo_url: 'http://i.imgur.com/9g1bKdR.jpg', year: 2016)
+
+Lookslike.create!(name: 'Tom Thibodeau',
+looks_like:'Looks like the guy in an antacid commercial uncomfortably eating a chili dog while pulling at his collar for relief.',
+photo_url: 'http://i.imgur.com/xYNkGVV.jpg', year: 2016)
+
+Lookslike.create!(name: 'Wade Phillips',
+looks_like:'Looks like the Bank Security guard who gives up his gun after robbers get a jump on him by putting a banana to his neck.',
+photo_url: 'http://i.imgur.com/QqVKb4m.jpg', year: 2016)
+
+Lookslike.create!(name: 'Paul Finebaum',
+looks_like:'Looks like the human version of the NCAA rule book.',
+photo_url: 'http://i.imgur.com/RA6pj6R.jpg', year: 2016)
+
+Lookslike.create!(name: 'Jim Caldwell',
+looks_like:'Looks like the guy answering the door at 3 am who instantly realizes the policeman there is going to give him life-changing bad news.',
+photo_url: 'http://i.imgur.com/yovQAgV.jpg', year: 2016)
+
+Lookslike.create!(name: 'Pete Carroll',
+looks_like:'Face looks like a mask a bank robber would wear.',
+photo_url: 'http://i.imgur.com/85Zvuu2.jpg', year: 2016)
+
+Lookslike.create!(name: 'Bartolo Colon',
+looks_like:'Looks like the last Mohican, who ate the second to last Mohican.',
+photo_url: 'http://i.imgur.com/A39m9ss.jpg', year: 2016)
+
+Lookslike.create!(name: 'Joe Torre',
+looks_like:'Looks like the guy who enters the sauna wearing a gold chain and takes off his towel in the middle of the sauna while walking towards the open bench across from you, and sits down next to Wade Phillips.',
+photo_url: 'http://i.imgur.com/7gcolQT.jpg', year: 2016)
+
+Lookslike.create!(name: 'Wade Phillips',
+looks_like:"Looks like a guy who can't figure out how the critters are getting under the fence.",
+photo_url: 'http://i.imgur.com/RNQ4L8V.jpg', year: 2016)
+
+Lookslike.create!(name: 'Steve Kerr',
+looks_like:'Looks like the guy who starts practicing his golf swing mid-conversation.',
+photo_url: 'http://i.imgur.com/AtbwRzh.jpg', year: 2016)
+
+Lookslike.create!(name: 'Greg Olsen',
+looks_like:'Looks like He-Man if he moved to Portland and opened a store that sells artisanal jams.',
+photo_url: 'http://i.imgur.com/jc7nUcq.jpg', year: 2016)
+
+Lookslike.create!(name: 'Jay Gruden',
+looks_like:'Looks like a flashy local realtor whose photo on the bus stop seat keeps getting defaced with horns and a mustache.',
+photo_url: 'http://i.imgur.com/PrwsoEA.jpg', year: 2016)
+
+Lookslike.create!(name: 'Lane Kiffin',
+looks_like:'Is the boater who throttles up in a no wake zone because he sees Jim Tomsula fishing with a Snoopy pole in a row boat.',
+photo_url: 'http://i.imgur.com/vd42VWZ.jpg', year: 2016)
+
+Lookslike.create!(name: 'Brian Dawkins',
+looks_like:'Looks like the guy at church who points to the sky with his eyes closed biting his lip and crying.',
+photo_url: 'http://i.imgur.com/CP7VWdKm.jpg', year: 2016)
+
+Lookslike.create!(name: 'Matthew Stafford ',
+looks_like:'Looks like your friend\'s friend who uses your bathroom, comes out fifteen minutes later and asks, "Do you have a plunger?"',
+photo_url: 'http://i.imgur.com/qH4EntP.jpg', year: 2016)
+
+Lookslike.create!(name: 'Ty Lawson',
+looks_like:'Looks like the DirecTV installer who shows up to your house an hour late and claims he has the wrong equipment and has to reschedule.',
+photo_url: 'http://i.imgur.com/IFjg3sJ.jpg', year: 2016)
+
+Lookslike.create!(name: 'Russell Wilson ',
+looks_like:'Looks like a dolphin trainer.',
+photo_url: 'http://i.imgur.com/HGVnHIQ.jpg', year: 2016)
+
+Lookslike.create!(name: 'Wade Phillips',
+looks_like:'Looks like someone you would find on a bottle of BBQ Sauce.',
+photo_url: 'http://i.imgur.com/N2V2qs0.jpg', year: 2016)
+
+Lookslike.create!(name: 'Roy Williams',
+looks_like:'Looks like the curator of a small-town historical society who patiently gazes out the window for potential visitors all day, sighs, then flips the sign to "CLOSED."',
+photo_url: 'http://i.imgur.com/xZgZ7K2.jpg', year: 2016)
+
+Lookslike.create!(name: 'Dana Holgorsen',
+looks_like:'Looks like that guy your Dad punched at Steak N\' Shake when you were a kid.',
+photo_url: 'http://i.imgur.com/UO7jF00.jpg', year: 2016)
+
+Lookslike.create!(name: 'Barry Melrose',
+looks_like:'Looks like a former gladiator who won his freedom and now runs a gladiator ring of his own in a backwater town.',
+photo_url: 'http://i.imgur.com/VR3F614.jpg', year: 2016)
+
+Lookslike.create!(name: 'Kurt Warner',
+looks_like:'Looks like the man emerging from the pool in the "after" portion of the Rogaine ads.',
+photo_url: 'http://i.imgur.com/nwyJ7w0m.jpg', year: 2016)
+
+Lookslike.create!(name: 'Steve Kerr',
+looks_like:'Looks like the new young pastor at a church that slowly wins over the old people who don\'t like change.',
+photo_url: 'http://i.imgur.com/mDDUhz6m.jpg', year: 2016)
+
+Lookslike.create!(name: 'Jeff Fisher',
+looks_like:'Looks like a guy in a Lynyrd Skynyrd shirt shotgunning a beer on the infield of a NASCAR track.',
+photo_url: 'http://i.imgur.com/En7rkhgl.jpg', year: 2016)
+
+Lookslike.create!(name: 'Chris Kaman',
+looks_like:'Looks like the butler of a haunted mansion.',
+photo_url: 'http://i.imgur.com/iNSgNw4l.jpg', year: 2016)
+
+Lookslike.create!(name: 'Ned Yost',
+looks_like:'Looks like every white person\'s aunt.',
+photo_url: 'http://i.imgur.com/QuEvRATl.jpg', year: 2016)
+
+Lookslike.create!(name: 'Charlie Strong',
+looks_like:'Looks like his name is Charlie Strong.',
+photo_url: 'http://i.imgur.com/M6LZCC6l.jpg', year: 2016)
+
+Lookslike.create!(name: 'Roger Goodell',
+looks_like:'Looks like the pastor in a town that has banned dancing.',
+photo_url: 'http://i.imgur.com/XqtsPcMl.jpg', year: 2016)
+
+Lookslike.create!(name: 'Billy Donovan',
+looks_like:'Looks like a vampire who woke up in the wrong century.',
+photo_url: 'http://i.imgur.com/8Zchmt0l.jpg', year: 2016)
+
+Lookslike.create!(name: 'Jim Harbaugh',
+looks_like:'Looks like the guy who yells at his wife in front of everyone when they lose at Pictionary on game night.',
+photo_url: 'http://i.imgur.com/7rpS86Sl.jpg', year: 2016)
+
+Lookslike.create!(name: 'Romeo Crennel',
+looks_like:'Looks like he just watched his neighbor\'s dog get eaten by an alligator.',
+photo_url: 'http://i.imgur.com/ew7tvcyl.jpg', year: 2016)
+
+Lookslike.create!(name: 'Jeff Van Gundy',
+looks_like:'Looks like a nervous scientist in a Sci-Fi movie who, after a series of awkward exchanges with the hero, says "I don\'t get out much."',
+photo_url: 'http://i.imgur.com/9WvujN2l.jpg', year: 2016)
